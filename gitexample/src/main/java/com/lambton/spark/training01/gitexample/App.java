b5b3b1110a6d14823606f1d19663866142d8e6b7
PoojaNakar <46945168+PoojaNakar@users.noreply.github.com>
2019-02-19 19:22:38 -0500

    1st commit in git using eclipse

---
+++ gitexample/src/main/java/com/lambton/spark/training01/gitexample/App.java
@@ -0,0 +1,13 @@
+package com.lambton.spark.training01.gitexample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
